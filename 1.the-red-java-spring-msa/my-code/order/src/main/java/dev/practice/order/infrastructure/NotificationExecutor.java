af526c732fb6c19bd7d94918dfbb1b3065126c22
sinkyoungdeok <tlsrudejr123@nate.com>
2021-12-07 01:00:06 +0900

    [ADD] (주문 프로젝트 개발) Partner Domain 개발 - Application, Interface 구현

---
+++ 1.the-red-java-spring-msa/my-code/order/src/main/java/dev/practice/order/infrastructure/NotificationExecutor.java
@@ -0,0 +1,24 @@
+package dev.practice.order.infrastructure;
+
+import dev.practice.order.domain.notification.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class NotificationExecutor implements NotificationService {
+    @Override
+    public void sendEmail(String email, String title, String description) {
+        log.info("sendEmail");
+    }
+
+    @Override
+    public void sendKakao(String phoneNo, String description) {
+
+    }
+
+    @Override
+    public void sendSms(String phoneNo, String description) {
+
+    }
+}
